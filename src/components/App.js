4a32a3ee9a80bd4318abf01241d4ece995a9503a
malknaor <malknaor@gmail.com>
2019-10-24 13:46:51 +0300

    Added redux react-redux redux-thunk local-storage to the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import MainNavigation from './Naviagtion/MainNavigation';
+
 const App = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+
     return (
         <div className="app-container">
-            App
+            <MainNavigation />
         </div>
     );
 };
