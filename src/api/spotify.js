a88ae6db7be6948ba79050733022bbcc7aec2cc8
malknaor <malknaor@gmail.com>
2019-11-11 10:35:45 +0200

    Added request and response interceptors to axios spotify instance

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -1,5 +1,66 @@
 import axios from 'axios';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-export default axios.create({
+import localStorageService from '../Services/LocalStorageService';
+
+const spotify = axios.create({
     baseURL: 'http://localhost:8888'
 });
+
+// Request Interceptor
+spotify.interceptors.request.use(
+    config => {
+        const token = localStorageService.getToken();
+
+        if (token) {
+            config.headers['Authorization'] = 'Bearer ' + token;
+            // config.headers['Content-type'] = 'application/x-www-form-urlencoded';
+        }
+
+        return config;
+    },
+    error => {
+        Promise.reject(error);
+    }
+);
+
+// Response Interceptor
+spotify.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        const originalRequest = error.config;
+
+        console.log(error);
+
+        if (error.statusCode === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            return spotify.get('/refresh-token', {
+                "refresh_token": localStorageService.getRefreshToken()
+            })
+            .then( res => {
+                if (res.status === 200) {
+                    // Update new access_token in local storage
+                    localStorageService.setToken(res.data);
+
+                    // Change Authorization header
+                    spotify.defaults.headers.common['Authorization'] = 'Bearer ' + localStorageService.getToken();
+
+                    // Return originalRequest
+                    return spotify(originalRequest);
+                }
+            })
+        }
+
+        if (error.statusCode === 401 && originalRequest.url === 'http://127.0.0.1:8888/refresh_token') {
+            Router.push('/');
+            return Promise.reject(error);
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default spotify;
