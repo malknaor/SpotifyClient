b76d0e1439a7e79448deb465fd00268e54e414df
Naor Malka <malknaor@gmail.com>
2019-12-03 15:16:57 +0200

    Added progress bar,player reducer and actions and spotify web player script

--- src/components/MusicPlayerControl/AudioProgressBar.js
+++ src/components/MusicPlayerControl/AudioProgressBar.js
@@ -1,23 +1,20 @@
 import React from 'react';
 
+import ProgressBar from './ProgressBar';
 import './AudioProgressBar.css';
 
-class AudioProgressBar extends React.Component {
-    render() {
-        return (
-            <div className="audio-progress-container">
-                <div className="song-duration">
-                    <p>00:00:00</p>
-                </div>
-                <div className="progress-container">
-                    <div className="draggable-point"></div>
-                </div>
-                <div className="song-duration">
-                    <p>00:00:00</p>
-                </div>
+const AudioProgressBar = props => {
+    return (
+        <div className="audio-progress-container">
+            <div className="song-duration">
+                <p>{props.currentDuration? props.currentDuration : '0:00'}</p>
             </div>
-        );
-    }
-}
+            <ProgressBar />
+            <div className="song-duration">
+                <p>{props.songDuration? props.songDuration : '0:00'}</p>
+            </div>
+        </div>
+    );
+};
 
 export default AudioProgressBar;
---
+++ src/components/SpotifyWebPlayer/SpotifyWebPlayerScript.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Script from 'react-load-script';
+
+import localStorageService from '../../Services/LocalStorageService';
+
+class SpotifyWebPlayerScript extends React.Component {
+    onLoad = () => {
+        window.onSpotifyWebPlaybackSDKReady = () => {
+            const token = localStorageService.getToken();
+            const player = new window.Spotify.Player({
+                name: 'Web Playback SDK Quick Start Player',
+                getOAuthToken: cb => { cb(token); }
+            });
+
+            // Error handling
+            player.addListener('initialization_error', ({ message }) => { console.error(message); });
+            player.addListener('authentication_error', ({ message }) => { console.error(message); });
+            player.addListener('account_error', ({ message }) => { console.error(message); });
+            player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+            // Playback status updates
+            player.addListener('player_state_changed', state => { console.log(state); });
+
+            // Ready
+            player.addListener('ready', ({ device_id }) => {
+                console.log('Ready with Device ID', device_id);
+            });
+
+            // Not Ready
+            player.addListener('not_ready', ({ device_id }) => {
+                console.log('Device ID has gone offline', device_id);
+            });
+
+            // Connect to the player!
+            player.connect();
+        };
+    }
+
+    render() {
+        return (
+            <Script
+                url="https://sdk.scdn.co/spotify-player.js"
+                onLoad={this.onLoad}
+                onError={console.log}
+            >
+            </Script>
+        );
+    }
+}
+
+export default SpotifyWebPlayerScript;
