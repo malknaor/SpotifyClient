7034485eb4d35cd08ecff4821044f24c07faccac
Naor Malka <malknaor@gmail.com>
2019-12-30 11:45:13 +0200

    Fixed some general display issues

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import localStorageService from '../../Services/LocalStorageService';
 import { fetchUser } from '../actions/index';
@@ -11,14 +11,14 @@ import UserLibraryPage from '../Pages/UserLibraryPage/UserLibraryPage';
 import UserPage from '../Pages/UserAccountPage/UserAccountPage';
 import LoginPage from '../Pages/LoginPage/LoginPage';
 import MusicPlayerControls from '../MusicPlayerControl/MusicPlayerControls';
-import { 
-    ACCESS_TOKEN, 
+import {
+    ACCESS_TOKEN,
     REFRESH_TOKEN
 } from '../../constants/StorageKeys';
 import './App.css';
 
 class App extends React.Component {
-   componentDidMount() {
+    componentDidMount() {
         const urlParams = new URLSearchParams(window.location.search);
 
         // save to local storage
@@ -27,7 +27,7 @@ class App extends React.Component {
                 access_token: urlParams.get(ACCESS_TOKEN),
                 refresh_token: urlParams.get(REFRESH_TOKEN)
             }
-            
+
             localStorageService.setToken(tokenObj);
 
             // Update current user
@@ -43,12 +43,14 @@ class App extends React.Component {
             return (
                 <Router>
                     <div className="app-upper-container">
-                        <MainNavigation user={this.props.user}/>
+                        <MainNavigation user={this.props.user} />
                         <div className="app-page">
-                            <Route path="/" exact component={HomePage}></Route>
-                            <Route path="/search" component={SearchPage}></Route>
-                            <Route path="/mylibrary" component={UserLibraryPage}></Route>
-                            <Route path="/user" component={UserPage}></Route>
+                            <Switch>
+                                <Route path="/" exact component={HomePage}></Route>
+                                <Route path="/search" component={SearchPage}></Route>
+                                <Route path="/mylibrary" component={UserLibraryPage}></Route>
+                                <Route path="/user" component={UserPage}></Route>
+                            </Switch>
                         </div>
                     </div>
                     <div className="app-lower-container">
--- src/components/actions/UserFeedActions.js
+++ src/components/actions/UserFeedActions.js
@@ -1,13 +1,13 @@
-import { 
-    FETCH_USER, 
+import {
+    FETCH_USER,
     FETCH_RECENTLY_PLAYED,
     FETCH_USER_TOP_TRACKS,
     FETCH_USER_TOP_ARTISTS
 } from '../../constants/ActionTypes';
-import { 
+import {
     USER_ACCOUNT,
     RECENTLY_PLAYED,
-    USER_TOP_X 
+    USER_TOP_X
 } from '../../constants/Routes';
 import spotify from '../../api/spotify';
 import requestBody from './requestBody';
@@ -19,33 +19,41 @@ export const fetchUser = () => async dispatch => {
 };
 
 export const fetchRecentlyPlayed = () => async dispatch => {
-    const res = await spotify.get(RECENTLY_PLAYED, requestBody);
+    try {
+        const res = await spotify.get(RECENTLY_PLAYED, requestBody);
 
-    dispatch({ type: FETCH_RECENTLY_PLAYED, payload: res.data });
+        dispatch({ type: FETCH_RECENTLY_PLAYED, payload: res.data });
+    } catch (error) {
+        console.log("TCL: fetchRecentlyPlayed -> error", error)
+    }
 };
 
 export const fetchUserTopX = type => async dispatch => {
-    const { headers } = requestBody;
-    
-    if (type === 'tracks') {
-        const res = await spotify.get(USER_TOP_X, {
-            headers: { 
-                ...headers,
-                type: type
-            }
-        });
-    
-        dispatch({ type: FETCH_USER_TOP_TRACKS, payload: res.data });
-    } 
-    
-    if (type === 'artists') {
-        const res = await spotify.get(USER_TOP_X, {
-            headers: { 
-                ...headers,
-                type: type
-            }
-        });
-    
-        dispatch({ type: FETCH_USER_TOP_ARTISTS, payload: res.data });
+    try {
+        const { headers } = requestBody;
+
+        if (type === 'tracks') {
+            const res = await spotify.get(USER_TOP_X, {
+                headers: {
+                    ...headers,
+                    type: type
+                }
+            });
+
+            dispatch({ type: FETCH_USER_TOP_TRACKS, payload: res.data });
+        }
+
+        if (type === 'artists') {
+            const res = await spotify.get(USER_TOP_X, {
+                headers: {
+                    ...headers,
+                    type: type
+                }
+            });
+
+            dispatch({ type: FETCH_USER_TOP_ARTISTS, payload: res.data });
+        }
+    } catch (error) {
+        console.log(`TCL: fetchUserTopX -> ${type} -> error`, error);
     }
 };
